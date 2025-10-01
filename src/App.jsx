1c47acd8021e0305184c270eac46e7d32f82e80c
HASHIM <ashhim4ever@gmail.com>
2025-10-01 22:58:07 +0530

    Updated project

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from "./components/About";
 import Courses from "./components/Courses";
 import Gallery from "./components/Gallery";
 import Contact from "./components/Contact";
+import Testimonials from "./components/Testimonials";
 import Footer from "./components/Footer";
 
 function App() {
